b866ef185797783648e05a685e1789dbe1588bcf
Josareta <josareta@gmail.com>
2023-09-28 00:21:13 +0200

    Añadir Ejercicio 06

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,4 @@
-// Escribe tu código aquí:
+// Dividir subcadenas
+let cadena = "Dividiremos la cadena en una array con el método split";
+let array = cadena.split(" ") //Entre paréntesis va el separador que tendrá en cuenta el método split, por donde se harán las divisiones.
+console.log(array)
