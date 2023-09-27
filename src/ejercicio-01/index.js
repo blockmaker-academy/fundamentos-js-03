6a6d231984847afbe2cb82a90cba4d579a2d0390
Josareta <josareta@gmail.com>
2023-09-27 22:37:15 +0200

    Añadir Ejercicio 01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,4 @@
 // Escribe tu código aquí:
+const cadenaOriginal = "Hola Mundo";
+const cadenaMayuscula = cadenaOriginal.toUpperCase();
+console.log("Aquí tiene su cadena en mayúsculas: " + cadenaMayuscula)
