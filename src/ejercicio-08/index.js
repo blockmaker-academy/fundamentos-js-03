015ceac5416d16d3ab46ac0331331318db3eb157
Josareta <josareta@gmail.com>
2023-09-28 00:59:58 +0200

    Añadir Ejercicio 08

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,5 @@
-// Escribe tu código aquí:
+// Verificar Inclusión
+let texto = "Verificando si existe subcadena con el método includes";
+let buscar = "subcadena";
+let veredicto = texto.includes(buscar); //Este método arroja true o false. 
+console.log(veredicto)
