a64d22ce033a1e47e4165b28c0829cf8c142397c
Josareta <josareta@gmail.com>
2023-09-28 01:11:31 +0200

    Añadir Ejercicio 09

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Eliminar espacios
+var texto = "    Eliminar espacios principio y fin     ";
+//  Se usará el método .trim
+var textoCorregido = texto.trim()
+
+console.log(textoCorregido);
+
