30841af60f80431606bab54327cb8274e173de01
Josareta <josareta@gmail.com>
2023-09-28 13:20:07 +0200

    Añadir Ejercicio 15

--- src/ejercicio-15/index.js
+++ src/ejercicio-15/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Redondear decimales. El método Math.round(), redondea al entero más cercano
+
+var num = 3.5
+const numRedon = Math.round(num);
+console.log(numRedon);
+
+
