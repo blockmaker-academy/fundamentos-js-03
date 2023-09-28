d1e4c98d1586b37d3173ef498c2b298704769d9d
Josareta <josareta@gmail.com>
2023-09-28 13:42:47 +0200

    Añadir Ejercicio 16

--- src/ejercicio-16/index.js
+++ src/ejercicio-16/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Redondeo abajo. Con el método Math.floor(), redondeamos los decimales hacia abajo.
+
+let valor = 7.9;
+let redondeo = Math.floor(valor);
+
+console.log("Redondeo inferior del número " + valor + " = " + redondeo)
+
