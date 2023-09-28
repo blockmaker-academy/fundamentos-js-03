87bad66bec7b80377b91b7b506813e1535ecf8df
Josareta <josareta@gmail.com>
2023-09-28 16:51:48 +0200

    Añadir Ejercicio 22

--- src/ejercicio-22/index.js
+++ src/ejercicio-22/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+// Eliminar primer elemento de  un Array. Se utiliza el método .shift()
+let animales = 
+["Perro", "Gato", "Burro"]
+let quitar = animales.shift()
+
+console.log("Quitando "+ quitar + " de la lista. La nueva lista queda: " + animales)
