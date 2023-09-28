addaf99ebc335a90b226be5f0cebee33682700ec
Josareta <josareta@gmail.com>
2023-09-28 17:40:08 +0200

    Añadir Ejercicio 25

--- src/ejercicio-25/index.js
+++ src/ejercicio-25/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+// Filtrar números. Se utiliza el método .filter()
+
+let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let pares = lista.filter((num) => num % 2 === 0)
+
+console.log("Aquí tiene su lista de números pares "+ pares)
