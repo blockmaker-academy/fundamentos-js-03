09006eece6051ffb6b2764aea6212f035bceff51
Josareta <josareta@gmail.com>
2023-09-28 13:04:28 +0200

    Añadir Ejercicio 14

--- src/ejercicio-14/index.js
+++ src/ejercicio-14/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Verificar número entero con el método Number.isInteger(), arrojará un valor Booleano
+
+let num1 = 42
+let num2 = 3.14
+
+console.log(num1 + " es entero?: " + Number.isInteger(num1));
+console.log(num2 + " es entero?: " + Number.isInteger(num2))
+
+
+
