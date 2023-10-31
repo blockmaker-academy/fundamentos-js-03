38d99debac33ddb399b460d9ee15948d7444a463
XaviBarragan <xbarragan73@gmail.com>
2023-10-31 18:58:14 +0100

    Añadir ejercicio-08

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Ejercicio 8 - Verificar Inclusión
+
+// Dada una cadena de texto y una subcadena, utiliza el método includes() para verificar si la cadena principal contiene la subcadena. Luego, imprime el resultado.
+const frase = 'JavaScript es divertido'
+const terminoBuscado = 'divertido'
+const contieneTermino = frase.includes(terminoBuscado)
+
+console.log(contieneTermino)
+// Resultado: true
+
