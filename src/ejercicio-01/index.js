d166dbc6949eedf65c93b4f428bcc70220fe9658
XaviBarragan <xbarragan73@gmail.com>
2023-10-31 09:43:57 +0100

    Añadir ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+// Ejercicio 1 - Convertir a Mayúsculas
+
+// Dada una cadena de texto en minúsculas, utiliza el método toUpperCase() para convertirla en mayúsculas. Luego, imprime el resultado.
+const cadenaOriginal = 'hola mundo'
+const cadenaEnMayusculas = cadenaOriginal.toUpperCase()
+
+// Muestro en servidor node
+console.log(cadenaEnMayusculas)
+// Resultado => 'HOLA MUNDO'
