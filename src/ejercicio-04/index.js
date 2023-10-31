ef5aaa45b8ce869922121ac67cd8a730f9e88195
XaviBarragan <xbarragan73@gmail.com>
2023-10-31 18:05:12 +0100

    Añadir ejercicio-04

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+// Ejercicio 4 - Encontrar Subcadena
+
+// Dada una cadena de texto y una subcadena, utiliza el método indexOf() para encontrar la posición de la subcadena en la cadena principal. Luego, imprime el resultado.
+const frase = 'Ubi non est ordo, ibi est confusio.'
+const posicion = frase.indexOf('est')
+const ultimaPosicion = frase.lastIndexOf('est')
+
+console.log(posicion + ' y ' + ultimaPosicion)
+// Resultado: 8 y 22
