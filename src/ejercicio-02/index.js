38e407f86012690a62ce6002acce37dd9f55c89b
XaviBarragan <xbarragan73@gmail.com>
2023-10-31 17:43:11 +0100

    Añadir ejercicio-02

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 2 - Convertir a Minúsculas
+
+// Dada una cadena de texto en mayúsculas, utiliza el método toLowerCase() para convertirla en minúsculas. Luego, imprime el resultado.
+const cadenaOriginal = 'HOLA A TODOS'
+const cadenaEnMinusculas = cadenaOriginal.toLowerCase()
+
+console.log(cadenaEnMinusculas)
+// Resultado: 'hola a todos'
