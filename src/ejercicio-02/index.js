d90da83747c9e5486827a18dc2959ceac292417d
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-11-01 18:44:11 +0100

    Añadir Commit ejercicio-02

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,5 @@
 // Escribe tu código aquí:
+const originalString = 'HOLA PEPE LUIS'
+const lowerCaseString = originalString.toLowerCase()
+
+console.log(lowerCaseString)//Resultado esperado 'hola pepe luis'
