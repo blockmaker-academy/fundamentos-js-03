fb6477be3f8727fd3743d9a9f6b36fbad983da18
Darkrake <rachel_1723@hotmail.com>
2023-11-02 19:31:47 +0100

    Añadir ejer. 4-8

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,5 @@
 // Escribe tu código aquí:
+const sentence = 'El perro es fiel y el gato es independiente.'
+const replacedSentence = sentence.replace('perro', 'mono')
+
+console.log(replacedSentence) // Resultado esperado: "El pájaro es fiel y el gato es independiente."
