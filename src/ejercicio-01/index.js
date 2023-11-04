69865970aa4d13a53f8a6f3cbcd1dfa9dbbebb43
johannymk7 <johannymk7@github.com>
2023-11-04 19:28:02 +0100

    Añadir ejercicio 01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,5 @@
 // Escribe tu código aquí:
+ const cadenaOriginal = 'hola mundo'
+ const cadenaEnMayuscula = cadenaOriginal.toUpperCase()
+
+ console.log(cadenaEnMayuscula) // Resultado esperado: "HOLA MUNDO" 
