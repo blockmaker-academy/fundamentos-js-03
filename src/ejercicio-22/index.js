52102b3bc096054c6a06e0cfc9e27ede8aa9c27f
Darkrake <rachel_1723@hotmail.com>
2023-11-06 21:21:22 +0100

    Añadir ejer.22

--- src/ejercicio-22/index.js
+++ src/ejercicio-22/index.js
@@ -1,1 +1,6 @@
 // Escribe tu código aquí:
+const animals = ['dog', 'cat', 'rabbit']
+const removedAnimal = animals.shift()
+
+console.log(removedAnimal) // Resultado esperado: "dog"
+console.log(animals) 
