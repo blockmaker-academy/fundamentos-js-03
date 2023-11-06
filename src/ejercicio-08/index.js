a240ff73a93e7ea3979f05de4cf62cb16aabc73e
johannymk7 <johannymk7@github.com>
2023-11-07 00:03:47 +0100

    Añadir ejercicio-08

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,6 @@
 // Escribe tu código aquí:
+const phrase = 'JavaScript es divertido'
+const searchTerm = 'divertido'
+const containsterm = phrase.includes(searchTerm)
+
+console.log(containsterm)  // Resultado esperado: True
