2964c8b636c5b0d4cb4d7942b484de92ea1bc92e
johannymk7 <johannymk7@github.com>
2023-11-09 10:13:51 +0100

    Añadir ejercicio-21

--- src/ejercicio-21/index.js
+++ src/ejercicio-21/index.js
@@ -1,1 +1,5 @@
 // Escribe tu código aquí:
+const fruits = ['apple', 'banana', 'orange']
+fruits.unshift('grape', 'kiwi')
+
+console.log(fruits)  // Resultado esperado: ["grape", "kiwi", "apple", "banana", "orange"]
