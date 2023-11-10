304bd5949588fb2c4b960ddfa90bc6a34accf0e3
johannymk7 <johannymk7@github.com>
2023-11-10 11:07:36 +0100

    Añadir ejercicio-24

--- src/ejercicio-24/index.js
+++ src/ejercicio-24/index.js
@@ -1,1 +1,5 @@
 // Escribe tu código aquí:
+const sentence = 'esta es una oreción de ejemplo.'
+const words = sentence.split(' ')
+
+console.log(words)  // Resultado esperado: ["Esta", "es", "una", "oracion", "de","ejemplo."]
